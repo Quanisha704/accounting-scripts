914d0b6be74043310682dbd655201bdf147a6abe
Quanisha Williams <ms_q_williams@yahoo.com>
2021-03-09 04:07:07 +0000

    Added additional dictionaries

--- melon_info.py
+++ melon_info.py
@@ -1,10 +1,10 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_names, melon_seedlessness, melon_prices, melon_flesh_color, melon_rind_color, melon_weight
 
 
-def print_melon(name, seedless, price):
+def print_melon(name, seedless, price, flesh_color, rind_color, weight):
     """Print each melon with corresponding attribute information."""
 
     have_or_have_not = 'have'
@@ -15,4 +15,5 @@ def print_melon(name, seedless, price):
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i], 
+                melon_flesh_color[i], melon_rind_color[i], melon_weight[i])
