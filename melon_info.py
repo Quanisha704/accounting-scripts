a5330f036e11963190c9b8e334412c14bdf0072b
Quanisha Williams <ms_q_williams@yahoo.com>
2021-03-09 04:27:48 +0000

    Added additional parameters with print statement

--- melon_info.py
+++ melon_info.py
@@ -1,17 +1,19 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices, melon_flesh_color, melon_rind_color, melon_weight
+from melons import melon_names, melon_seedlessness, melon_prices,melon_flesh_color, melon_rind_color, melon_weight
 
 
-def print_melon(name, seedless, price, flesh_color, rind_color, weight):
+def print_melon(name, seedless, price, flesh_color = None, rind_color = None, weight = None):
     """Print each melon with corresponding attribute information."""
 
     have_or_have_not = 'have'
     if seedless:
         have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    
+    
+    print(f'{name}\nSeedless:{seedless}\nPrice:{price}\nflesh_color:{flesh_color}\nweight:{weight}\nrind_color:{rind_color}')    
+    #print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
 for i in melon_names:
